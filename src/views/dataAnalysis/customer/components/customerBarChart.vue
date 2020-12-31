ac8c5c4d5907b2381e6d66adc4265199a72d23c9
xyc <1365884247@qq.com>
2020-12-31 10:15:40 +0800

    数据分析顾客分析模板搭建

---
+++ src/views/dataAnalysis/customer/components/customerBarChart.vue
@@ -0,0 +1,41 @@
+<!--顾客年龄段柱状图-->
+<template>
+  <div>
+    <BarChart
+      id="customerBarChart"
+      width="100%"
+      height="100%"
+      :option="option"
+    />
+  </div>
+</template>
+
+<script>
+import BarChart from '@/components/barChart'
+export default {
+  components: {
+    BarChart
+  },
+  data() {
+    return {
+      option: {
+        xAxis: {
+          type: 'category',
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [
+          {
+            data: [120, 200, 150, 80, 70, 110, 130],
+            type: 'bar'
+          }
+        ]
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
